51ee6319cb9e888bd4a0146f8b4a2c81299116b3
Andréas DADING <moundss@gmail.com>
2019-05-12 12:50:57 +0100

    :construction: WIP.

---
+++ src/DependencyHandling.DataAccess/Contact.Repository.fs
@@ -0,0 +1,15 @@
+﻿namespace DependencyHandling.DataAccess
+
+module ContactRepository =
+    open System
+    open DependencyHandling.Domain
+
+    let saveContact (contact : Contact) =
+        () |> Ok
+        
+    let loadContact (id : Guid) =
+        {
+            Id = Guid.NewGuid()
+            Name = "zappy"
+            Address = "1229 ggg"
+        } |> Ok
